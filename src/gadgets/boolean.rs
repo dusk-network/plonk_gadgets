9e1e89dd5d82c315739b227328a0c4c153977f37
CPerezz <c.perezbaro@gmail.com>
2020-03-11 10:44:30 +0100

    Refactor Boolvar

--- src/gadgets/boolean.rs
+++ src/gadgets/boolean.rs
@@ -1,17 +1,9 @@
-use algebra::fields::jubjub::fq::Fq;
-use plonk::cs::constraint_system::{LinearCombination, Variable};
+use plonk::cs::constraint_system::Variable;
 
 /// Represents a Variable that has already
 /// been constrained to be either one or zero.
 #[derive(Clone, Copy, Eq, PartialEq, Hash)]
 pub struct BoolVar(pub(crate) Variable);
-
-impl From<BoolVar> for LinearCombination<Fq> {
-    fn from(v: BoolVar) -> LinearCombination<Fq> {
-        let lc: LinearCombination<Fq> = v.0.into();
-        lc
-    }
-}
 
 impl Into<Variable> for BoolVar {
     fn into(self) -> Variable {
